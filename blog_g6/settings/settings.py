6b143371746bdef6aaba37804b8bcf33e6c62b8e
martin-b-07e5 <martin.bergagno@gmail.com>
2021-12-10 21:54:39 -0300

    we are using mysql now instead of sqlLite ✅

--- blog_g6/settings/settings.py
+++ blog_g6/settings/settings.py
@@ -80,10 +80,22 @@ WSGI_APPLICATION = 'blog_g6.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'g6db',
+        'USER': 'grupo6',
+        'PASSWORD': 'holagrupo6',
+        'HOST': '127.0.0.1',
+        # 'PORT': '5432',
+        'PORT': '3306',
     }
 }
 
